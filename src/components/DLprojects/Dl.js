86d73a5d7a82d7c262e8ef64cae031999d5cbbea
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2024-07-16 02:16:42 +0530

    updates

--- src/components/DLprojects/Dl.js
+++ src/components/DLprojects/Dl.js
@@ -16,6 +16,38 @@ function Dl() {
                         <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/text_to_vid/U-net" target="_blank"><img src={github} alt="hi" /></a>
                     </div>
                 </div>
+                <div class="dl_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="dl_card-image"/> */}
+                    <div class="dl_card-content" >
+                        <h2 class="dl_card-title">Text to Speech</h2>
+                        <p class="dl_card-text">Implementing tts using NVdia TacoTron-2 model on LJspeech dataset</p>
+                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/text_to_speech" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
+                <div class="dl_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="dl_card-image"/> */}
+                    <div class="dl_card-content" >
+                        <h2 class="dl_card-title">Chat with PDF (CLI)</h2>
+                        <p class="dl_card-text">Implementing Llama model on PDF data and using LLM for question answering. Works on CLI</p>
+                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/Basics/llama" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
+                <div class="dl_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="dl_card-image"/> */}
+                    <div class="dl_card-content" >
+                        <h2 class="dl_card-title">Chat with PDF (Web)</h2>
+                        <p class="dl_card-text">Implementing Llama model on PDF data and using LLM for question answering. Works on Web</p>
+                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/Basics/chat" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
+                <div class="dl_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="dl_card-image"/> */}
+                    <div class="dl_card-content" >
+                        <h2 class="dl_card-title">Chat with CSV (API)</h2>
+                        <p class="dl_card-text">Question answering with csv using OpenAi API key</p>
+                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/Basics/chat" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
                 
             </div>
 
--- src/components/MLprojects/Ml.js
+++ src/components/MLprojects/Ml.js
@@ -13,7 +13,7 @@ function Ml() {
                     <div class="ml_card-content" >
                         <h2 class="ml_card-title">K-NN From Scratch</h2>
                         <p class="ml_card-text">Classifying HandWirtten Digit MNIST dataset using KNN implemented using pandas (From scratch)</p>
-                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/text_to_vid/U-net" target="_blank"><img src={github} alt="hi" /></a>
+                        <a href="https://github.com/biswajit-github-2022/RKMVERI_2ND/tree/main/class/ML/SS_assignments_1" target="_blank"><img src={github} alt="hi" /></a>
                     </div>
                 </div>
                 <div class="ml_card" >
@@ -46,6 +46,46 @@ function Ml() {
                         <h2 class="ml_card-title">MultiDimension Scaling</h2>
                         <p class="ml_card-text">Implementing Multi Dimension Scaling For Visualization of Higher Dimension MNIST HD dataset</p>
                         <a href="https://github.com/biswajit-github-2022/RKMVERI_2ND/tree/main/class/ML/SS_assignments_5" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
+                <div class="ml_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="ml_card-image"/> */}
+                    <div class="ml_card-content" >
+                        <h2 class="ml_card-title">Denoising Image</h2>
+                        <p class="ml_card-text">Using KPCA(rbf), KPCA(wavelet) and PCA to denoise Images, by using Patch based Approach On high quality images </p>
+                        <a href="https://github.com/biswajit-github-2022/RKMVERI_2ND/tree/main/class/ML/ML_project/work" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
+                <div class="ml_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="ml_card-image"/> */}
+                    <div class="ml_card-content" >
+                        <h2 class="ml_card-title">Hous Price Prediction</h2>
+                        <p class="ml_card-text">Using Regression models to predict house prices with more than 40 features. </p>
+                        <a href="https://github.com/biswajit-github-2022/PRODIGY_ML_01" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
+                <div class="ml_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="ml_card-image"/> */}
+                    <div class="ml_card-content" >
+                        <h2 class="ml_card-title">Mall Customer Segregation</h2>
+                        <p class="ml_card-text">Using K-means Algorithm to segregate mall customers . </p>
+                        <a href="https://github.com/biswajit-github-2022/PRODIGY_ML_02" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
+                <div class="ml_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="ml_card-image"/> */}
+                    <div class="ml_card-content" >
+                        <h2 class="ml_card-title">Cats vs Dogs</h2>
+                        <p class="ml_card-text">Classifying Cats and Dogs images using SVM classifier. Then using PCA for visualization </p>
+                        <a href="https://github.com/biswajit-github-2022/PRODIGY_ML_03" target="_blank"><img src={github} alt="hi" /></a>
+                    </div>
+                </div>
+                <div class="ml_card" >
+                    {/* <img src="image1.jpg" alt="Image 1" class="ml_card-image"/> */}
+                    <div class="ml_card-content" >
+                        <h2 class="ml_card-title">Hand gesture Detection</h2>
+                        <p class="ml_card-text">Using CNN to detect 10 types of handgesture from images . </p>
+                        <a href="https://github.com/biswajit-github-2022/PRODIGY_ML_04" target="_blank"><img src={github} alt="hi" /></a>
                     </div>
                 </div>
                 
