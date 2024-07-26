3b798b36660ba734a89a65f64850a6706ab8442d
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2024-07-27 01:33:40 +0530

    new updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,65 +3,30 @@ import github from "../../icons/github.svg";
 import web from "../../icons/web.svg";
 function Dl() {
     return (
-        <div className="abt_body">
-            <div className="abt_heading">
-                Deep Learning Projects
-            </div>
 
-            <div class="abt_container">
-                <div class="abt_card" >
-                    {/* <img src="image1.jpg" alt="Image 1" class="abt_card-image"/> */}
-                    <div class="abt_card-content" >
-                        <h2 class="abt_card-title">U-net From scratch</h2>
-                        <p class="abt_card-text">Implementing U-net from scratch using tenserflow on red blood cell image dataset for segmentation</p>
-                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/text_to_vid/U-net" target="_blank"><img src={github} alt="hi" /></a>
-                    </div>
-                </div>
-                <div class="abt_card" >
-                    {/* <img src="image1.jpg" alt="Image 1" class="abt_card-image"/> */}
-                    <div class="abt_card-content" >
-                        <h2 class="abt_card-title">Text to Speech</h2>
-                        <p class="abt_card-text">Implementing tts using NVdia TacoTron-2 model on LJspeech dataset</p>
-                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/text_to_speech" target="_blank"><img src={github} alt="hi" /></a>
-                    </div>
-                </div>
-                <div class="abt_card" >
-                    {/* <img src="image1.jpg" alt="Image 1" class="abt_card-image"/> */}
-                    <div class="abt_card-content" >
-                        <h2 class="abt_card-title">Chat with PDF (CLI)</h2>
-                        <p class="abt_card-text">Implementing Llama model on PDF data and using LLM for question answering. Works on CLI</p>
-                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/Basics/llama" target="_blank"><img src={github} alt="hi" /></a>
-                    </div>
-                </div>
-                <div class="abt_card" >
-                    {/* <img src="image1.jpg" alt="Image 1" class="abt_card-image"/> */}
-                    <div class="abt_card-content" >
-                        <h2 class="abt_card-title">Chat with PDF (Web)</h2>
-                        <p class="abt_card-text">Implementing Llama model on PDF data and using LLM for question answering. Works on Web </p>
-                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/Basics/chat" target="_blank"><img src={github} alt="hi" /></a>
-                    </div>
-                </div>
-                <div class="abt_card" >
-                    {/* <img src="image1.jpg" alt="Image 1" class="abt_card-image"/> */}
-                    <div class="abt_card-content" >
-                        <h2 class="abt_card-title">Chat with CSV (API)</h2>
-                        <p class="abt_card-text">Question answering with csv using OpenAi API key</p>
-                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/Basics/chat" target="_blank"><img src={github} alt="hi" /></a>
-                    </div>
-                </div>
-                <div class="abt_card" >
-                    {/* <img src="image1.jpg" alt="Image 1" class="abt_card-image"/> */}
-                    <div class="abt_card-content" >
-                        <h2 class="abt_card-title">Gemini chatbot</h2>
-                        <p class="abt_card-text">Used Astro framework and gemini api for chatbot, live using vercel</p>
-                        <a href="https://ai-chat-app-rho-ten.vercel.app/" target="_blank"><img src={web} alt="hi" /></a>
-                        <a href="https://github.com/biswajit-github-2022/term_peoject/tree/main/Basics/chat" target="_blank"><img src={github} alt="hi" /></a>
-                    </div>
-                </div>
+        <div className="abt_main">
+            <div className="abt_heading">
+                About Me
+            </div>
+            <div className="abt_body">
+                <h1>Education</h1>
+                <p>
+                • Currently In 2nd Year of my <b>M.Sc in Data Science</b>  from <b>Ramakrishna Mission Vivekananda and Educational University</b> 
+                </p>
+                <p>
+                • Completed <b>B.Sc in Computer Science</b> from <b>Panskura Banamali College</b> under <b>Vidyasagar University</b> -- CGPA : 8.9
+                </p>
+                <p>
+                • Completed <b>Higher Secondary</b> from <b>Panskura Bradley Birt High School</b> in Pure Science  -- Percentage : 87%
+                </p>
+
+                <h1>Skills</h1>
+                    <p> <b>DataBases</b> : Mysql , MongoDB, Neo4j</p>
+                    <p> <b>Data Science</b> : Machine Learning, Deep Learning, NLP, AI</p>
                 
             </div>
-
         </div>
+
     );
 }
 
