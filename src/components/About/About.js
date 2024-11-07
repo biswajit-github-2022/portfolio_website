d6ee461951620f1ef04b92212e2a58016ef1bb9c
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2024-11-07 13:20:58 +0530

    new

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,49 +22,68 @@ function Dl() {
                     </div>
                     <div className="card_edu1">
                         <span className="text_edu1">
-                         • M.Sc. in Big data Analytics&nbsp;<br />
-                         • @Ramakrishna Mission Vivekananda and Educational University <br />
-                        • Aug,2023 - Present <br />
-                        • 8.5 SGPA (as of 2nd sem)
+                            • M.Sc. in Big data Analytics&nbsp;<br />
+                            • @Ramakrishna Mission Vivekananda and Educational University <br />
+                            • Aug,2023 - Present <br />
+                            • 8.5 SGPA (as of 2nd sem)
                         </span>
                         <img className="img_edu1" src={logo} alt="" />
                     </div>
                     <div className="card_edu2">
                         <span className="text_edu2">
-                        • B.Sc. in Computer Science&nbsp;<br />
-                        • @Panskura Banamali College (VidyaSagar University) <br /> 
-                        • dec,2020 - Apr,2023 <br />
-                        • 8.9 CGPA
+                            • B.Sc. in Computer Science&nbsp;<br />
+                            • @Panskura Banamali College (VidyaSagar University) <br />
+                            • dec,2020 - Apr,2023 <br />
+                            • 8.9 CGPA
                         </span>
                         <img className="img_edu2" src={logo2} alt="" />
                     </div>
                     <div className="card_edu3">
                         <span className="text_edu3">
-                        • Higher Secondary in Pure Science&nbsp;<br />
-                        •  @Panskura Bradley Birt High School <br/>
-                        • May,2018 - Jan,2020 <br />
-                        • 87.48% in PCMB
+                            • Higher Secondary in Pure Science&nbsp;<br />
+                            •  @Panskura Bradley Birt High School <br />
+                            • May,2018 - Jan,2020 <br />
+                            • 87.48% in PCMB
                         </span>
-                        <img className="img_edu3" src={logo3}   alt="" />
+                        <img className="img_edu3" src={logo3} alt="" />
                     </div>
-
                 </div>
 
 
-                {/* <h1>Education</h1>
-                <p>
-                • Currently In 2nd Year of my <b>M.Sc in Data Science</b>  from <b>Ramakrishna Mission Vivekananda and Educational University</b> 
-                </p>
-                <p>
-                • Completed <b>B.Sc in Computer Science</b> from <b>Panskura Banamali College</b> under <b>Vidyasagar University</b> -- CGPA : 8.9
-                </p>
-                <p>
-                • Completed <b>Higher Secondary</b> from <b>Panskura Bradley Birt High School</b> in Pure Science  -- Percentage : 87%
-                </p>
 
-                <h1>Skills</h1>
-                    <p> <b>DataBases</b> : Mysql , MongoDB, Neo4j</p>
-                    <p> <b>Data Science</b> : Machine Learning, Deep Learning, NLP, AI</p> */}
+
+                {/* <div className="skill">
+
+                    <div className="heading_edu">
+                        Skills
+                    </div>
+
+                    <div className="skill_1">
+                        <div>
+                            Databases
+                        </div>
+                        <div className="new">
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            <span className="a">a</span>
+                            
+                        </div>
+
+                    </div>
+                </div> */}
+
+
+
 
             </div>
         </div>
